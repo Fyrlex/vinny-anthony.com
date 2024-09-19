adf0fb1f49437ebda77513123de54ff3a228e797
Jaren Goldberg <fyrlex@gmail.com>
2024-09-19 10:11:06 -0400

    fix: calendar dates

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import { fly } from 'svelte/transition';
   export let currentDate!: Date;
   export let shows!: { title: string; date: Date }[];
 
@@ -58,10 +59,11 @@
   {#each weeks as week}
     {#each week as day}
       <div
-        class="hover:cursor-pointer hover:text-cyan-500 hover:bg-orange-300 duration-300 md:text-xl select-none p-1 md:p-2 text-center ring-2 ring-orange-300 rounded-md
+        transition:fly={{ delay: 2200 * weeks.indexOf(week), duration: 1200, y: 50 }}
+        class="hover:cursor-pointer hover:text-cyan-500 duration-300 md:text-xl select-none p-1 md:p-2 text-center ring-2 ring-orange-300 rounded-md
         {shows.some(show => show.date.getDate() === day.getDate() && show.date.getMonth() === day.getMonth())
           ? 'bg-emerald-300 hover:bg-emerald-500 hover:ring-emerald-500 hover:text-slate-900'
-          : ''}
+          : 'hover:bg-orange-300'}
         {day.getDate() === currentDate.getDate() ? 'text-cyan-500 bg-orange-300' : ''}
         {day.getMonth() !== currentDate.getMonth() ? 'text-gray-700' : ''}"
       >
