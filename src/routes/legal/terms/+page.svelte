a3c8db35edb1ea4803050a8af58fbbf7bb26c7f2
Jaren Goldberg <fyrlex@gmail.com>
2024-10-24 00:50:02 -0400

    feat: policies

---
+++ src/routes/legal/terms/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Metadata from '../../../components/Metadata.svelte';
+</script>
+
+<svelte:head>
+  <Metadata url="/legal/terms" />
+</svelte:head>
+
+<div class="mx-5 md:mx-20 text-slate-300">
+  <h1>Terms of Service</h1>
+  <p>
+    Welcome to vinnyanthony.live. By using our website, you agree to comply with and be bound by the following terms and
+    conditions of use. Please review the following terms carefully.
+  </p>
+
+  <h2>1. Acceptance of Terms</h2>
+  <p>
+    By accessing and using this website, you accept and agree to be bound by the terms and provision of this agreement.
+    In addition, when using these particular services, you shall be subject to any posted guidelines or rules applicable
+    to such services.
+  </p>
+
+  <h2>2. Description of Service</h2>
+  <p>
+    Vinnyanthony.live provides users with access to a variety of resources, including the purchase of products. These
+    products are integrated with Stripe for payment processing.
+  </p>
+
+  <h2>3. Return Policy</h2>
+  <p>
+    If you are not satisfied with your purchase, you may return the product by contacting us at <a
+      href="mailto:me@vinny-anthony.com">me@vinny-anthony.com</a
+    >. Please include your order details and reason for return in your email.
+  </p>
+
+  <h2>4. Privacy Policy</h2>
+  <p>
+    Your use of the website is also subject to our Privacy Policy. Please review our Privacy Policy, which also governs
+    the website and informs users of our data collection practices.
+  </p>
+
+  <h2>5. Changes to Terms</h2>
+  <p>
+    We reserve the right to change these terms at any time. Any changes will be posted on this page, and it is your
+    responsibility to review these terms periodically.
+  </p>
+
+  <h2>6. Contact Information</h2>
+  <p>
+    If you have any questions about these Terms, please contact us at <a href="mailto:me@vinny-anthony.com"
+      >me@vinny-anthony.com</a
+    >.
+  </p>
+</div>
+
+<style>
+  * {
+    font-family: 'Times New Roman', Times, serif;
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 1.75;
+    margin-top: 1.25rem;
+  }
+
+  h1 {
+    font-size: 3rem;
+    font-weight: 600;
+    margin-top: 3rem;
+  }
+
+  h2 {
+    font-size: 2.25rem;
+    font-weight: 600;
+    margin-top: 2.25rem;
+  }
+</style>
