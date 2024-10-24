86a1f6cf2bf908a65e7b5d5f6004c9a2a3c09746
Jaren Goldberg <fyrlex@gmail.com>
2024-10-24 12:54:17 -0400

    fix: products

--- src/lib/getProductInfo.ts
+++ src/lib/getProductInfo.ts
@@ -1,12 +1,12 @@
 const productPrices = {
-  'hoodie-s': 'price_1QCCLkH5WejaCk0DDPwPnzG4',
-  'hoodie-m': 'price_1QCCMiH5WejaCk0D8z1L5gDm',
-  'hoodie-l': 'price_1QCCLAH5WejaCk0DYAltrTZ0',
-  'hoodie-xl': 'price_1QCCNUH5WejaCk0DvgrmARJu',
-  't-shirt-s': 'price_1QCCJRH5WejaCk0DSoSZPAWb',
-  't-shirt-m': 'price_1QCCH8H5WejaCk0D0ECnR4Zp',
-  't-shirt-l': 'price_1QCCFJH5WejaCk0DZhnNLJ0c',
-  't-shirt-xl': 'price_1QCCIjH5WejaCk0DfocegqXv',
+  'hoodie-s': 'price_1QCCPMH5WejaCk0DvCbf5mRp',
+  'hoodie-m': 'price_1QCCPLH5WejaCk0DVULOkSDY',
+  'hoodie-l': 'price_1QCCPOH5WejaCk0DRNX0wx3E',
+  'hoodie-xl': 'price_1QCCPHH5WejaCk0DT3XWtkwZ',
+  't-shirt-s': 'price_1QCCPPH5WejaCk0DmZrjlKAB',
+  't-shirt-m': 'price_1QCCPRH5WejaCk0DZxUsflLs',
+  't-shirt-l': 'price_1QCCPTH5WejaCk0DfZqldqRp',
+  't-shirt-xl': 'price_1QCCPQH5WejaCk0DUN7JKMAA',
 };
 
 import Hoodie from '$lib/assets/merch/tshirtfront.png';
